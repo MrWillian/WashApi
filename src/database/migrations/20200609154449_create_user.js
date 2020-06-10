c9cdfd468baeac33cd7e12ab3612b1c67ca057bf
MrWilllian <willian.dev10@gmail.com>
2020-06-10 15:08:07 -0300

    refactor: add user migration

---
+++ src/database/migrations/20200609154449_create_user.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('users', function(table) {
+    table.increments();
+    table.string('name').notNullable();
+    table.string('email').notNullable();
+    table.string('password').notNullable();
+    table.string('cellphone');
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
