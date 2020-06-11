8434e5fb56c01f4da91ff18e280d90cabeb788b0
MrWilllian <willian.dev10@gmail.com>
2020-06-11 10:24:01 -0300

    refactor: add routes

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+const UserController = require('./controllers/UserController');
+const ClientController = require('./controllers/ClientController');
+const CompanyController = require('./controllers/CompanyController');
+
+// Middleware
 router.use(function (req, res, next) {
-  console.log("/" + req.method);
   next();
 });
 
-router.get('/', (request, response) => {
-  console.log("/" + request);
-  return response.json({
-    name: 'Willian Marciel'
-  });
-});
+// User Routes
+router.get('/users', UserController.index);
+router.post('/user', UserController.create);
+router.get('/user', UserController.show);
+
+// Client Routes
+router.get('/clients', ClientController.index);
+router.post('/client', ClientController.create);
+router.get('/client', ClientController.show);
+
+// Company Routes
+router.get('/companies', CompanyController.index);
+router.post('/company', CompanyController.create);
+router.get('/company', CompanyController.show);
 
 module.exports = router;
