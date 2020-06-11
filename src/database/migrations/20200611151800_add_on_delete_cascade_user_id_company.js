276773bdc46150ae558fa35b1853d3a67750dafb
MrWilllian <willian.dev10@gmail.com>
2020-06-11 16:10:43 -0300

    refactor: add on delete cascade company migration

---
+++ src/database/migrations/20200611151800_add_on_delete_cascade_user_id_company.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+  return knex.schema.alterTable("companies", (table) => {
+    table.dropForeign("user_id");
+    table.foreign("user_id").references("users.id").onDelete("CASCADE");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.alterTable("companies", (table) => {
+    table.dropForeign("user_id");
+    table.foreign("user_id").references("users.id").onDelete("NO ACTION");
+  });
+};
