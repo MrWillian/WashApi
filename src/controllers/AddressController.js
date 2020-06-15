aa7a692bc7fbecfa16f0adefd0ffefe24a20a8df
MrWilllian <willian.dev10@gmail.com>
2020-06-15 14:59:28 -0300

    refactor: implement address repository methods

--- src/controllers/AddressController.js
+++ src/controllers/AddressController.js
@@ -1,30 +1,19 @@
-const connection = require("../database/connection.js");
+const addressRepository = require("../repositories/AddressRepository.js");
 
 module.exports = {
   async index(request, response) {
-    const addresses = await connection('addresses').select('*');
-    return response.json(addresses);
+    return await addressRepository.index(request, response);
   },
 
   async create(request, response) {
-    const { country, state, city, cep, number, user_id } = request.body;
-
-    await connection('addresses').insert({
-      country, state, city, cep, number, user_id
-    });
-
-    return response.status(200).send();
+    return await addressRepository.create(request, response);
   },
 
   async show(request, response) {
-    const { user_id } = request.body;
-    const address = await connection('addresses').where('user_id', user_id).first();
-    return response.json(address);
+    return await addressRepository.show(request, response);
   },
 
   async delete(request, response) {
-    const { id } = request.body;
-    await connection('addresses').where('id', id).delete();
-    return response.json({"status_code":204, "result":true});
+    return await addressRepository.delete(request, response);
   }
 }
