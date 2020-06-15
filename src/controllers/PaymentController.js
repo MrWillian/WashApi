e9967f3dd7c7e49bffaab6fc3d18867f56f20f3f
MrWilllian <willian.dev10@gmail.com>
2020-06-15 14:59:47 -0300

    refactor: implement payment repository methods

--- src/controllers/PaymentController.js
+++ src/controllers/PaymentController.js
@@ -1,26 +1,19 @@
-const connection = require("../database/connection.js");
+const paymentRepository = require("../repositories/PaymentRepository.js");
 
 module.exports = {
   async index(request, response) {
-    const payments = await connection('payments').select('*');
-    return response.json(payments);
+    return await paymentRepository.index(request, response);
   },
 
   async create(request, response) {
-    const { card_number, card_name, expire_date, cvv, user_id } = request.body;
-    await connection('payments').insert({ card_number, card_name, expire_date, cvv, user_id });
-    return response.json({"status_code": 204, "result": true});
+    return await paymentRepository.create(request, response);
   },
 
   async show(request, response) {
-    const { user_id } = request.body;
-    const payment = await connection('payments').where('user_id', user_id).first();
-    return response.json(payment);
+    return await paymentRepository.show(request, response);
   },
 
   async delete(request, response) {
-    const { id } = request.body;
-    await connection('payments').where('id', id).delete();
-    return response.json({"status_code":204, "result":true});
+    return await paymentRepository.delete(request, response);
   }
 }
