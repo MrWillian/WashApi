d32a9a1c4f1180159e26532d0bddb591d3aa94dc
MrWilllian <willian.dev10@gmail.com>
2020-06-15 10:12:50 -0300

    refactor: add Address and Payment routes

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const UserController = require('./controllers/UserController');
 const ClientController = require('./controllers/ClientController');
 const CompanyController = require('./controllers/CompanyController');
+const AddressController = require('./controllers/AddressController');
+const PaymentController = require('./controllers/PaymentController');
 
 // Middleware
 router.use(function (req, res, next) {
@@ -14,6 +16,7 @@ router.use(function (req, res, next) {
 router.get('/users', UserController.index);
 router.post('/user', UserController.create);
 router.get('/user', UserController.show);
+router.delete('/user', UserController.delete);
 
 // Client Routes
 router.get('/clients', ClientController.index);
@@ -24,5 +27,17 @@ router.get('/client', ClientController.show);
 router.get('/companies', CompanyController.index);
 router.post('/company', CompanyController.create);
 router.get('/company', CompanyController.show);
+
+// Address Routes
+router.get('/addresses', AddressController.index);
+router.post('/address', AddressController.create);
+router.get('/address', AddressController.show);
+router.delete('/address', AddressController.delete);
+
+// Payment Routes
+router.get('/payments', PaymentController.index);
+router.post('/payment', PaymentController.create);
+router.get('/payment', PaymentController.show);
+router.delete('/payment', PaymentController.delete);
 
 module.exports = router;
