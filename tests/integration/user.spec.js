af8a81f015d6aea9d396419a687663b00a3127e6
MrWilllian <willian.dev10@gmail.com>
2020-06-15 13:07:17 -0300

    test: add delete user test

--- tests/integration/user.spec.js
+++ tests/integration/user.spec.js
@@ -3,22 +3,31 @@ const app = require('../../src/app');
 const connection = require('../../src/database/connection');
 
 describe('User', () => {
-  beforeEach(async () => {
-    await connection.migrate.latest();
-  });
+  beforeEach(async () => { await connection.migrate.latest(); });
 
-  afterAll(() => {
-    connection.destroy();
+  afterAll(async () => { 
+    await connection.migrate.rollback();
+    connection.destroy(); 
   });
 
   it('should be able to create a new User', async () => {
-    const response = await request(app).post('/user').send({
+    const response = await createUser();
+    expect(response.body).toHaveProperty('name');
+  });
+
+  it('should be able to delete a User', async () => {
+    await createUser();
+    const response = await request(app).delete('/user').send({ id: 1 });
+    expect(response.body).toHaveProperty('result', true);
+  });
+
+  async function createUser() {
+    return await request(app).post('/user').send({
+      id: 1,
       name: "teste1",
       email: "teste1@gmail",
       password: "123456",
       cellphone: "77999999999"
     });
-
-    expect(response.body).toHaveProperty('name');
-  });
+  }
 });
