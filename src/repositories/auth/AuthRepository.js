08fd49b4dd4486b3a62b15a8bb5d86379b5794a1
MrWilllian <willian.dev10@gmail.com>
2020-06-19 15:32:00 -0300

    refactor: add password compare to login

--- src/repositories/auth/AuthRepository.js
+++ src/repositories/auth/AuthRepository.js
@@ -1,14 +1,22 @@
 const connection = require("../../database/connection.js");
+const bcrypt = require('bcrypt');
 
 module.exports = {
   async login(request, response) {
-    const { email } = request.body;
+    const { email, password: decryptedPassword } = request.body;
+
     let query = connection('users')
-      .select('id','name','email','cellphone')
+      .select('id','name','email','cellphone', 'password', 'initialization_vector')
       .where('email', email).first();
 
-    query.then(function (result) {
-      return response.status(200).json(result);
+    query.then(async function (result) {
+      bcrypt.compare(decryptedPassword, result.password, function(err, res) {
+        if (res == true)
+          return response.status(200).json(result);
+        
+        return response.status(401).send();
+      });
+
     }).catch(function (error) { return response.status(400).json(error); });
   },
 }
