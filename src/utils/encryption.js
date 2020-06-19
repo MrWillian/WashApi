5bfc82f2f2c938d4f437754169e5c61fffae87ba
MrWilllian <willian.dev10@gmail.com>
2020-06-19 15:29:10 -0300

    refactor: change encrypt to bcrypt

--- src/utils/encryption.js
+++ src/utils/encryption.js
@@ -1,22 +1,27 @@
-const crypto = require('crypto');
-const algorithm = 'aes-256-cbc';
-const key = crypto.randomBytes(32);
-const iv = crypto.randomBytes(16);
+const bcrypt = require('bcrypt');
+const saltRounds = 10; 
 
 module.exports = {
-  encrypt(text) {
-    let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
-    let encrypted = cipher.update(text);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+  async encrypt(password) {
+    return await genEncrypt(password);
   },
 
-  decrypt(text) {
-    let iv = Buffer.from(text.iv, 'hex');
-    let encryptedText = Buffer.from(text.encryptedData, 'hex');
-    let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
-    let decrypted = decipher.update(encryptedText);
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
-    return decrypted.toString();
-  }
+  async decrypt(password, hash) {
+    bcrypt.compare(password, hash, function(err, res) {
+      if (res == true) {
+        return true;
+      }
+      // else wrong password
+    });
+  },
+}
+
+async function genEncrypt(password) {
+  bcrypt.genSalt(saltRounds, (err, salt) => { 
+    const result = bcrypt.hash(password, salt, (err, hash) => {
+      console.log(hash);
+      return { salt, hash };
+    });
+    console.log('result', result);
+  });
 }
