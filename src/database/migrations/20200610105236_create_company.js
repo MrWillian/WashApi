99be159209b3d4176f4c5dc3511dc5ddfc8ebd11
MrWilllian <willian.dev10@gmail.com>
2020-06-22 16:05:00 -0300

    refactor: remove not nullable from cnpj column

--- src/database/migrations/20200610105236_create_company.js
+++ src/database/migrations/20200610105236_create_company.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('companies', function(table) {
     table.increments();
-    table.string('cnpj').notNullable();
+    table.string('cnpj');
     table.string('tellphone');
     table.integer('user_id').unsigned();
     table.foreign("user_id").references("users.id").onDelete("CASCADE");
